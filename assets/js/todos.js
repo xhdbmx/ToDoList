692ca4da6c1a0ff8216134886aeca028758333cb
xhdbmx <xhdbmx@gmail.com>
2020-01-15 19:25:00 -0500

    initial upload

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+//Check off Specific Todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete todo
+$("ul").on("click", "span", function(){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//Adding todos
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle()
+});
